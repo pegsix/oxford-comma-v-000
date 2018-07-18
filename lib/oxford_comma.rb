d78b6d16c3366d579e35fe116c1aa37ee79e812a
Peggy Lee <peggyleesix@gmail.com>
2018-07-18 21:38:49 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-  array.join(", ")
+  if 2 values in array
+  array.join(" and ")
+elsif 
+  if 3 values in array
+  array.join(", ") between array(0) and array(1)
+  array.join(", and") between array(1) and array(2)
+
 end
