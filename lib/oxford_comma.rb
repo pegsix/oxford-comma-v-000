3b1d431f99b06381f55c99e04c4f22d8cf644b21
Peggy Lee <peggyleesix@gmail.com>
2018-07-18 22:07:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+ if array.length == 1
+   return "#{array[0]}"
+elsif array.length == 2
+    return array.join(" and ")
+elsif array.length >= 3
+   last_array_spot = "and #{array[-1]}"
+   array.pop
+   array << (last_array_spot)
+   return array.join(", ")
+end
 end
